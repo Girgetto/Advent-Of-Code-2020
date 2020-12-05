492bc8a5781266fdb092756fcb4ace1f8c745bd4
Giorgio Grassini <giorgiograssini@gmail.com>
2020-12-05 12:02:02 +0100

    almost

--- 04/index.js
+++ 04/index.js
@@ -1,17 +1,15 @@
-const { create } = require("domain");
-
 const fields = ["ecl", "pid", "eyr", "hcl", "byr", "iyr", "hgt"];
 const conditions = {
-  byr: { min: 1920, max: 2020 },
+  byr: { min: 1920, max: 2002 },
   iyr: { min: 2010, max: 2020 },
   eyr: { min: 2020, max: 2030 },
   hgt: {
-    cm: { min: 150, max: 190 },
+    cm: { min: 150, max: 193 },
     in: { min: 59, max: 76 },
   },
-  hcl: new RegExp(),
-  ecl: new RegExp(/amb|blu|brn|gry|grn|hzl|oth/, "g"),
-  pid: new RegExp(/@"^\d$"/, "g"),
+  hcl: new RegExp(/^(#[a-z0-9]{6})?/, "g"),
+  ecl: new RegExp(/(amb|blu|brn|gry|grn|hzl|oth)/, "g"),
+  pid: new RegExp(/^[0-9]+$/, "g"),
 };
 
 const createArray = (data) => {
@@ -19,11 +17,13 @@ const createArray = (data) => {
   let str = "";
   for (i = 0; i < data.length; i++) {
     if (data[i] === "") {
-      array.push(str);
+      array.push(str.trim());
       str = "";
     } else if (i === data.length - 1) {
       str += data[i];
-      array.push(str);
+      if (str !== "") {
+        array.push(str.trim());
+      }
     } else {
       str += data[i] + " ";
     }
@@ -40,13 +40,36 @@ const firstPart = (data) => {
   return createArray(data).filter((el) => el.match(regex));
 };
 
+const mapConditions = (element) => {
+  let [property, value] = element.trim().split(":");
+  if (property === "cid") return true;
+  if (value.includes("in")) {
+    return (
+      conditions[property].in.max >= value.replace("in", "") &&
+      value.replace("in", "") >= conditions[property].in.min
+    );
+  }
+  if (value.includes("cm") && conditions[property].cm) {
+    return (
+      conditions[property].cm.max >= value.replace("cm", "") &&
+      value.replace("cm", "") >= conditions[property].cm.min
+    );
+  }
+
+  return conditions[property].min
+    ? conditions[property].max >= value && value >= conditions[property].min
+    : value.match(conditions[property]) !== null &&
+        value.match(conditions[property])[0] !== "";
+};
+
 const secondPart = (data) => {
   const passportsFiltered = firstPart(data);
-  return passportsFiltered.filter((el) => {
-    el.split(" ").map((element) => {
-      conditions[element.split(":")[0]];
-    });
-  });
+  return passportsFiltered.filter((passport) =>
+    passport
+      .split(" ")
+      .map((element) => mapConditions(element))
+      .every((test) => test)
+  );
 };
 
 module.exports = {
